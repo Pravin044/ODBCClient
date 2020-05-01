f1f74464f4c4e2719fba4f854fb0e0d455f1584d
pravin kharade <pravin.pk044@gmail.com>
2020-05-01 13:38:10 +0530

    getDriver list and dsn names list

--- ODBCClient/MainWindow.xaml.cs
+++ ODBCClient/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Odbc;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,81 @@ namespace ODBCClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        OdbcConnection odbcCon = new OdbcConnection();
+        OdbcCommand OdbcCommand = new OdbcCommand();
+        OdbcDataAdapter OdbcData;
+        DataSet dataSet;
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void connectToDB()
+        {
+            try
+            {
+                odbcCon.Open();
+                if(odbcCon.State.ToString()=="Open")
+                {
+                    MessageBox.Show("connected");
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            //connectToDB();
+            var ODBCdrivers = GetOdbcDriverNames();
+            var DsnNames = EnumDsn();
+        }
+
+
+        /// <summary>
+        /// Gets the ODBC driver names from the registry.
+        /// </summary>
+        /// <returns>a string array containing the ODBC driver names, if the registry key is present; null, otherwise.</returns>
+        public static string[] GetOdbcDriverNames()
+        {
+            string[] odbcDriverNames = null;
+            using (RegistryKey localMachineHive = Registry.LocalMachine)
+            using (RegistryKey odbcDriversKey = localMachineHive.OpenSubKey(@"SOFTWARE\ODBC\ODBCINST.INI\ODBC Drivers"))
+            {
+                if (odbcDriversKey != null)
+                {
+                    odbcDriverNames = odbcDriversKey.GetValueNames();
+                }
+            }
+
+            return odbcDriverNames;
+        }
+
+
+        private List<string> EnumDsn()
+        {
+            List<string> list = new List<string>();
+            list.AddRange(EnumDsn(Registry.CurrentUser));
+            list.AddRange(EnumDsn(Registry.LocalMachine));
+            return list;
+        }
+
+        private IEnumerable<string> EnumDsn(RegistryKey rootKey)
+        {
+            RegistryKey regKey = rootKey.OpenSubKey(@"Software\ODBC\ODBC.INI\ODBC Data Sources");
+            if (regKey != null)
+            {
+                foreach (string name in regKey.GetValueNames())
+                {
+                    string value = regKey.GetValue(name, "").ToString();
+                    yield return name;
+                }
+            }
         }
     }
 }
